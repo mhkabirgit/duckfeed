5f064805e6645796e90f398d91a334c957becfa7
mhkabirgit <40705644+mhkabirgit@users.noreply.github.com>
2018-08-28 22:09:47 -0700

    Refactored package.json to deploy in heroku

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(session({
   key: 'user_sid',
